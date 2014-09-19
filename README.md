ca88e16f65720588ddf8848e84f493e90128382d
David Tudury <david.tudury@gmail.com>
2014-09-19 09:23:46 -0700

    putting the pieces together

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 cryptopotamus
 =========
+__Crypto__graphic __P__assword __T__arget __M__anagement __S__ystem
 secure client-side password generation and management
 
-this is running at:
-http://cryptopotamus.jit.su/
+a PBKDF2 hash used to seed a Blum Blum Shub RNG.  The RNG is used by a Regex based generator to generate passwords.
+
--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 var crypto = require('crypto');
-var bn = require('bignum');
+var BN = require('bignum');
 var RandExp = require('randexp');
+var Clipboard = require("copy-paste").noConflict().silent();
 
 var config = require('./sample.json');
 
-//product of 2 bn.prime(128) results (for blum blum shub)
-var M = bn('78098118281233005579972958143295588852526594861094468258402109666655482488793');
+//product of 2 BN.prime(128) results (for blum blum shub)
+var M = BN('78098118281233005579972958143295588852526594861094468258402109666655482488793');
 var seed;
 function prng(a, b) {
-    seed = bn.powm(seed, 2, M);
+    seed = BN.powm(seed, 2, M);
     return seed.mod(1 + b - a).add(a).toNumber();
 }
-RandExp.setRandIntFunction(prng);
+RandExp.prototype.randInt = prng;
 
-var password = "asdf1234";
-
-var entry = config[0];
-
-var hash = crypto.pbkdf2Sync(password, entry.version, entry.iterations, 32);
-seed = bn.fromBuffer(hash);
 
 function shuffle(str) {
     var output = "";
@@ -29,20 +24,29 @@ function shuffle(str) {
     return output;
 }
 
-function renderTarget(obj) {
+function generate(generator) {
     var output = "";
-    if (obj.sequence) {
-        obj.sequence.forEach(function (step) {
-            output += renderTarget(step);
+    if (generator.sequence) {
+        generator.sequence.forEach(function (subGenerator) {
+            output += generate(subGenerator);
         });
-    } else if (obj.regex) {
-        output = RandExp.randexp(RegExp(obj.regex));
+    } else if (generator.regex) {
+        output = RandExp.randexp(RegExp(generator.regex));
     }
-    if (obj.shuffle) {
+    if (generator.shuffle) {
         output = shuffle(output);
     }
     return output;
 }
 
-var output = renderTarget(entry.generator);
-console.log(output);
+function renderTarget(target, password) {
+    var salt = target.username + '\0' + target.website + '\0' + target.version;
+    var hash = crypto.pbkdf2Sync(password, salt, target.iterations, 32);
+    seed = BN.fromBuffer(hash);
+    return generate(target.generator);
+}
+
+var password = "asdf1234";
+var key = renderTarget(config[0], password);
+console.log(key);
+Clipboard.copy(key)
